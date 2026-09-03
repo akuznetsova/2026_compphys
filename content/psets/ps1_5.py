ae87924a4e1e5cf33bb1a4298427c1292f405737
Aleksandra <astro.kuznetsova@gmail.com>
2026-09-03 17:01:07 -0400

    whoops

--- content/psets/ps1_5.py
+++ content/psets/ps1_5.py
@@ -1,17 +1,11 @@
 #a)
 def catalan(n):
-    if n ==0:
-        return 1
-    else:
-        return (4*n - 2) * catalan(n-1) // (n+1)
+    # ..
 #b)
 c_100 = catalan(100)
 print(c_100)
 #c)
 def g(m,n):
-    if n == 0:
-        return m
-    else:
-        return g( n, m % n )
+    # ..
 #d)
 print( g(108,192))
